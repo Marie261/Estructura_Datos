296ad654d73e10d1c3de36c9b3ec8ed8de824b69
ValeB03 <valenescobailarin@gmail.com>
2026-08-23 18:29:06 -0500

    Segundo commit cola

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Estructura_Datos.PILA;
+
+Pila<int> pila = new Pila<int>();
+
+pila.Push(10);
+pila.Push(20);
+pila.Push(30);
+
+Console.WriteLine("Elemento superior"+pila.Peek());
+Console.WriteLine("Elemento eliminado"+pila.Pop());
+Console.WriteLine("Elemento superior despues del Pop"+pila.Peek());
+Console.WriteLine("Tamaño de la Pila: "+ pila.Tamaño());
